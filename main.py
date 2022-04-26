a9a01807560728879b86ebcd73896037074d9229
Shravan Kumar <>
2022-04-26 07:46:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+# select number of players
+n = int(input("Select Number of Players(Example: 10)\n : "))
+
+players = list(range(1, n + 1))
+
+shuffle(players)
+
+# print(players)
+
+while players:
+    text = input("Next Player")  # or raw_input in python2
+    if text == "":
+        print(f"your batting order: {players.pop(0)}")
+    else:
+        print("you typed some text before pressing enter")
